eba59b3f359a4db78958ea7c9880600a9617dcf8
staslif <149203375+staslif@users.noreply.github.com>
2024-12-24 10:57:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    private const string src1 = @"C:\Users\Admin\Desktop\2 курс\4.docx";
+    private const string src2 = @"C:\Users\Admin\Desktop\2 курс\9.docx";
+
+    private const string dest1 = @"C:\Users\Admin\Desktop\2 курс\copy4.docx";
+    private const string dest2 = @"C:\Users\Admin\Desktop\2 курс\copy9.docx";
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            // Запускаем копирование файлов 
+            await Task.WhenAll(CopyFileAsync(src1, dest1), CopyFileAsync(src2, dest2));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    private static async Task CopyFileAsync(string source, string destination)
+    {
+        // Проверяем исходный файл
+        if (File.Exists(source))
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            try
+            {
+                // Копируем файл
+                FileInfo fileInfo = new FileInfo(source);
+                await Task.Run(() => fileInfo.CopyTo(destination, true));
+                stopwatch.Stop();
+
+                Console.WriteLine($"Файл '{Path.GetFileName(source)}' успешно скопирован в '{destination}'");
+                Console.WriteLine($"Время выполнения копирования: {stopwatch.Elapsed}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при копировании файла '{Path.GetFileName(source)}': {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Файл '{Path.GetFileName(source)}' не найден.");
+        }
+    }
+}
